fcbf7d5e67db1a4fef8970990e6764c12ec36d36
alim1496 <alim1496@gmail.com>
2020-09-07 21:10:25 +0600

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/confusion",
+  facebook: {
+    clientId: "870050333529694",
+    clientSecret: "5bcc20a4f0c7cb3f208baec60a823bdd",
+  },
+};
